997fd420b9a361880b1befb42701131306267e38
Golfins studios <165297153+Golfinsstd@users.noreply.github.com>
2024-06-29 12:54:36 -0300

    Update GSTD-commonwire.h

--- src/GSTD-commonwire.h
+++ src/GSTD-commonwire.h
@@ -2,24 +2,35 @@
 #define GSTD_COMMONWIRE
 
 #include <Arduino.h>
+#include <string>
 
+using namespace std;
 
 class commonFUNC
 
 {
 private:
 public:
-    commonFUNC( int gpio,int temp);
-    void blink();
-    void fade();
+  commonFUNC(int gpio, int temp);
+  void blink();
+  void fade();
 };
 
 class motorPIN
 {
-  private:
-  public:
+private:
+public:
   motorPIN(int gpioM1);
   void motorwrite(int mapwm);
   void printspeed();
 };
+class buzzerATCH
+{
+private:
+
+public:
+ buzzerATCH(int gpioBZ);
+ void noteWRITE(string nota);
+};
+
 #endif
